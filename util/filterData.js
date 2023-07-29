71e6df800fb3468332b6fffe658ee23af16a1d16
Rhythm Shandlya <armaanbgp@gmail.com>
2023-07-29 15:03:16 +0530

    filter data utility

---
+++ util/filterData.js
@@ -0,0 +1,7 @@
+exports.filterData = (obj, ...allowedFields) => {
+  let newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
